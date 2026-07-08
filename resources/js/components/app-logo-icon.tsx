370e457fc0170db769a2805c65c7f32ce5a0abb6
Jack Torino <jvcktorino@gmail.com>
2026-07-08 12:06:19 +0800

    Fix logo path and remove invalid import

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,13 +1,11 @@
 import type { ImgHTMLAttributes } from 'react';
-// Vite will resolve this path during the build process
-import logo from '../../images/UVLOGO.svg';
 
 export default function AppLogoIcon(
     props: ImgHTMLAttributes<HTMLImageElement>,
 ) {
     return (
         <img
-            src={logo}
+            src="/images/UVLOGO.svg" // This is the correct path for public/ images
             alt="Logo"
             {...props}
             className={`h-10 w-10 ${props.className ?? ''}`}
