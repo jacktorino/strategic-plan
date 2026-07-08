883e2357fa5815f1d2e9c6d79d5d23c95304d3fe
Jack Torino <jvcktorino@gmail.com>
2026-07-08 12:03:00 +0800

    Add logo via Vite

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,11 +1,13 @@
 import type { ImgHTMLAttributes } from 'react';
+// Vite will resolve this path during the build process
+import logo from '../../images/UVLOGO.svg';
 
 export default function AppLogoIcon(
     props: ImgHTMLAttributes<HTMLImageElement>,
 ) {
     return (
         <img
-            src="/images/UVLOGO.svg"
+            src={logo}
             alt="Logo"
             {...props}
             className={`h-10 w-10 ${props.className ?? ''}`}
